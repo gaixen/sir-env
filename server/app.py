e413918fac80d3738f3101ba9903c9affc29554c
gaixen <sohamjee1447@gmail.com>
2026-04-06 23:10:18 +0530

    add sever scripts

---
+++ server/app.py
@@ -0,0 +1,12 @@
+from openenv_service import app
+import uvicorn
+
+
+def main() -> None:
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
+
+__all__ = ["app", "main"]
